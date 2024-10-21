2aef03ea24c2014ecca08ab7fee8e525bd0e7141
Anders <anders-code@users.noreply.github.com>
2024-10-21 07:33:10 -0700

    spicpu

---
+++ rtl/spi_cpu_6502.sv
@@ -0,0 +1,69 @@
+`resetall
+`default_nettype none
+
+`include "config.vh"
+`include "timescale.vh"
+
+module spi_cpu_6502 (
+    input  wire clk,
+    input  wire rst,
+    input  wire nmi,
+    input  wire irq,
+    output wire cs_n,
+    output wire mosi,
+    input  wire miso
+);
+
+wire [23:0]mem_addr;
+wire       mem_en = 1;
+wire       mem_wr;
+wire  [7:0]mem_wdata;
+wire  [7:0]mem_rdata;
+
+wire rdy;
+
+cpu_6502 cpu_inst (
+    .clk,
+    .reset (rst),
+    .AB  (mem_addr[15:0]),
+    .DI  (mem_rdata),
+    .DO  (mem_wdata),
+    .WE  (mem_wr),
+    .IRQ (irq),
+    .NMI (nmi),
+    .RDY (rdy)
+);
+
+assign mem_addr[23:16] = 0;
+
+reg last_we;
+reg [15:0]last_addr;
+always_ff @(posedge clk) begin
+    if (rdy) begin
+        last_we <= mem_wr;
+        last_addr <= mem_addr[15:0] + 1;
+    end
+end
+
+wire mem_burst = (mem_wr == last_we && mem_addr[15:0] == last_addr);
+
+spi_sram_master spi_sram_master_inst (
+    .clk,
+    .clk2  (!clk),
+    .rst,
+    .en    (1'b1),
+    .en2   (1'b1),
+    .ready (rdy),
+    .cs_n,
+    .miso,
+    .mosi,
+    .mem_addr,
+    .mem_en,
+    .mem_wr,
+    .mem_burst,
+    .mem_wdata,
+    .mem_rdata
+);
+
+endmodule
+`resetall
